9b1e9bfce9eb3033567f44d5ffb5e5ab507fa831
abhishek-codemojo <abhishek.tiwari@codemojo.io>
2018-01-20 10:22:45 +0530

    Token contract improved
    Documentation updated
    New Event for Token Issue
    Tokens are Burnable now
    Safety check on issueToken added
    IssueToken behaviour updated

--- contracts/Transcodium.sol
+++ contracts/Transcodium.sol
@@ -1,17 +1,18 @@
 pragma solidity ^0.4.11;
 
 
-import "/zeppelin-solidity/contracts/token/MintableToken.sol";
+import "/zeppelin-solidity/contracts/token/StandardToken.sol";
+import "/zeppelin-solidity/contracts/token/BurnableToken.sol";
 import "/zeppelin-solidity/contracts/ownership/Ownable.sol";
 
 /**
- * @title SimpleToken
- * @dev Very simple ERC20 Token example, where all tokens are pre-assigned to the creator.
- * Note they can later distribute these tokens as they wish using `transfer` and other
- * `StandardToken` functions.
+ * @title Transcodium
+ * @dev Transcodium is the original token that will be used across the Transcodium platform.
+ * This is a Burnable token with a supply of 120mn
  */
-contract Transcodium is Ownable, StandardToken  {
+contract Transcodium is Ownable, StandardToken, BurnableToken  {
 
+    event TokenIssued(address indexed from, address indexed to, uint256 value);
     string public constant name = "Transcodium";
     string public constant symbol = "TNS";
     uint8 public constant decimals = 18;
@@ -26,10 +27,13 @@ contract Transcodium is Ownable, StandardToken  {
         tokenIssuer = msg.sender;
     }
 
-    function issueTokens(address _to, uint256 _value) public onlyOwner {
-        balances[tokenIssuer] = balances[tokenIssuer].sub(_value);
+    function issueTokens(address _to, uint256 _value) public {
+        require(msg.sender == tokenIssuer);
+        require(balances[owner] > _value);
+
+        balances[owner] = balances[owner].sub(_value);
         balances[_to] = balances[_to].add(_value);
-        Transfer(tokenIssuer, _to, _value);
+        TokenIssued(tokenIssuer, _to, _value);
     }
 
     function changeTokenIssuer(address newTokenIssuer) public onlyOwner {
