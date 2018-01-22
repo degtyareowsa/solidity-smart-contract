410e5769c39e1dd4c6c173995381d2c4d205692c
abhishek-codemojo <abhishek.tiwari@codemojo.io>
2018-01-22 19:26:06 +0530

    Removal of TokenIssue mechanism

--- contracts/Transcodium.sol
+++ contracts/Transcodium.sol
@@ -12,32 +12,15 @@ import "/zeppelin-solidity/contracts/ownership/Ownable.sol";
  */
 contract Transcodium is Ownable, StandardToken, BurnableToken  {
 
-    event TokenIssued(address indexed from, address indexed to, uint256 value);
     string public constant name = "Transcodium";
     string public constant symbol = "TNS";
     uint8 public constant decimals = 18;
-
-    address public tokenIssuer;
 
     uint256 public constant INITIAL_SUPPLY = 120000000 * (10 ** uint256(decimals));
 
     function Transcodium() {
         totalSupply = INITIAL_SUPPLY;
         balances[msg.sender] = INITIAL_SUPPLY;
-        tokenIssuer = msg.sender;
-    }
-
-    function issueTokens(address _to, uint256 _value) public {
-        require(msg.sender == tokenIssuer);
-        require(balances[owner] > _value);
-
-        balances[owner] = balances[owner].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-        TokenIssued(tokenIssuer, _to, _value);
-    }
-
-    function changeTokenIssuer(address newTokenIssuer) public onlyOwner {
-        tokenIssuer = newTokenIssuer;
     }
 
 }
